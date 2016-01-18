f1e307a443029286244b835e346298bd24b23000
Maksym Bryzhko <maksym@transferwise.com>
2016-01-18 13:03:08 +0200

    Crowdin sync

---
+++ synk.sh
@@ -0,0 +1,4 @@
+#export CROWDINW_HOME=.
+export CROWDINW_PROJECT_API_KEY=21efd941b38122c3bc7bdfe2535421e8
+./crowdinw.groovy -t ./crowdin/crowdin.yaml.tpl upload source 
+./crowdinw.groovy -t ./crowdin/crowdin.yaml.tpl download 
